629a7c9ebd569ddbdc239754d656b7fa5cab5e14
yogeshasok <yogeshasok@gmail.com>
2016-08-09 17:48:42 +0530

    Add files via upload

---
+++ Integ.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Integ
+{
+    public static void main(String arg[])
+            {
+                Scanner d=new Scanner(System.in);
+                int num=Integer.parseInt(d.next());
+                System.out.println(num);
+              }
+ }
