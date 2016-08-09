6bfd5f2a073fe0b4e39e22450a3f7a6b948a7f9c
yogeshasok <yogeshasok@gmail.com>
2016-08-09 18:48:19 +0530

    Add files via upload

---
+++ Holiday.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Holiday
+{
+       public static void main(String arg[])
+            {
+      Scanner d=new Scanner(System.in);
+      String s=d.next();
+      boolean b=s.equalsIgnoreCase("sunday");
+      System.out.println(b);
+            }
+ }
