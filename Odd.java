2dfb09658b7a4c889e514a85834919d1e9c070f1
yogeshasok <yogeshasok@gmail.com>
2016-08-09 17:09:14 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class Odd
+{
+public static void main(String arg[])
+ {
+     Scanner d=new Scanner(System.in);
+     String str=d.next();
+     StringBuffer b=new StringBuffer(str);
+     str=b.reverse().toString();
+     System.out.println(str);
+     }
+     }
