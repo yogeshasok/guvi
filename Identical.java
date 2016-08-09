1b02c1e1a20f2c7b2cac0de6189c0525dde263b6
yogeshasok <yogeshasok@gmail.com>
2016-08-09 19:05:24 +0530

    Update Identical.java

--- Identical.java
+++ Identical.java
@@ -6,17 +6,22 @@ class Identical
      public static void main(String arg[])
             {
               Scanner d=new Scanner(System.in);
-               int num=d.nextInt();
-               int k=d.nextInt();
-               int i=0;
-                int count=0;
-               while(num>0)
-                   {
-                   i=num%10;
-                   if(i==k)
-                         count++;
-                   num=num/10;
-                    }
-             System.out.println(count);
+               int n=d.nextInt();
+               int count=0;
+               int arr[]=new int[n+1];
+               for(int i=0;i<n;i++)
+               arr[i]=d.nextInt();
+               for(int i=0;i<n-1;i++)
+                {
+                      for(int j=i+1;j<n;j++)
+                      {
+                           if(a[i)>a[j])
+                           count++;
+                      }
+                }
+                if(count>0)
+                System.out.println("Values not sorted");
+                else
+                System.out.println("Sorted");
          }
  }    
