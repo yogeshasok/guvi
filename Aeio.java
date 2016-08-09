d3c7d540166004e7c2437886ad02c5ef36f85844
yogeshasok <yogeshasok@gmail.com>
2016-08-09 18:39:38 +0530

    Add files via upload

---
+++ Aeio.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+import java.io.*;
+
+import java.lang.*;
+
+class Aeio
+{
+
+         public static void main(String arg[])
+ 
+               {
+     
+                 Scanner d=new Scanner(System.in);
+   
+                    String str=d.next();
+                    String out="";
+           
+     StringBuffer b=new StringBuffer(str);
+  
+                  str=b.reverse().toString();
+                  str=str.replace("a","");
+                   str=str.replace("e","");
+                   str=str.replace("i","");
+                   str=str.replace("o","");
+                   str=str.replace("u","");
+                    str=str.replace("A","");
+                   str=str.replace("E","");
+                   str=str.replace("I","");
+                   str=str.replace("O","");
+                   str=str.replace("U","");
+
+                   System.out.println(str);
+    
+               }
+     
+ }
